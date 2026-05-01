2029480332cd3ee17eb9066631a03ab32b917865
AureteProgrammer <CodingBlakeMorris@gmail.com>
2026-04-30 20:20:25 -0400

    creating initialPage function:

--- src/hooks/usePagination.ts
+++ src/hooks/usePagination.ts
@@ -4,6 +4,14 @@ export const usePagination = (totalItems: number, itemsPerPage: number) => {
     const [currentPage, setCurrentPage] = useState(1)
     const totalPages = Math.ceil(totalItems / itemsPerPage)
 
+    const initialPage = () => {
+        if (currentPage < 1) {
+            setCurrentPage(1)
+        } else if (currentPage > totalPages) {
+            setCurrentPage(totalPages)
+        }
+    }
+    
     const goToPage = (page: number) => {
         if (page < 1) {
             setCurrentPage(1)
@@ -22,11 +30,21 @@ export const usePagination = (totalItems: number, itemsPerPage: number) => {
         goToPage(currentPage - 1)
     }
 
+    useEffect(() => {
+        if (currentPage > totalPages) {
+            setCurrentPage(totalPages)
+        }
+    }, [totalItems, itemsPerPage, totalPages, currentPage])
+
+
     return {
         currentPage,
         totalPages, 
         goToPage,
         nextPage,
-        prevPage
+        prevPage,   
+        totalItems,
+        itemsPerPage
+
     }
 }
